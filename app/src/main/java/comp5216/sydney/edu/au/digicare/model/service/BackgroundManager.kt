3301c547ec775b1179d7787a7e66d7f77a0a9db9
ProfAttila7 <zdua0309@uni.sydney.edu.au>
2024-10-19 12:53:58 +1100

    Create BackgroundManager.kt

---
+++ app/src/main/java/comp5216/sydney/edu/au/digicare/model/service/BackgroundManager.kt
@@ -0,0 +1,26 @@
+package comp5216.sydney.edu.au.digicare.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+object BackgroundManager {
+
+    private const val TAG = "BackgroundManager"
+
+    fun scheduleTimeoutTask(context: Context) {
+        val timeoutWorkRequest = OneTimeWorkRequest.Builder(TimeoutWorker::class.java)
+            .setInitialDelay(10, TimeUnit.MINUTES)
+            .build()
+
+        WorkManager.getInstance(context).enqueue(timeoutWorkRequest)
+        Log.d(TAG, "Timeout task scheduled.")
+    }
+
+    fun cancelTimeoutTask(context: Context) {
+        WorkManager.getInstance(context).cancelAllWork()
+        Log.d(TAG, "Timeout task cancelled.")
+    }
+}
