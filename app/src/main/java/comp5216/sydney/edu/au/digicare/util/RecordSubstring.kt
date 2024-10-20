231edbbd2bb08d5589225c2240ee102c62f71cc5
Xinyuan Wang <xinyuan.wang01@gmail.com>
2024-10-20 15:48:27 +1100

    changed words limit displayed on history

--- app/src/main/java/comp5216/sydney/edu/au/digicare/util/RecordSubstring.kt
+++ app/src/main/java/comp5216/sydney/edu/au/digicare/util/RecordSubstring.kt
@@ -2,8 +2,8 @@ package comp5216.sydney.edu.au.digicare.util
 
 // substring first 50 characters
 fun displayFirst50Characters(text: String): String {
-    return if (text.length > 50) {
-        text.substring(0, 50)
+    return if (text.length > 150) {
+        text.substring(0, 150)
     } else {
         text
     }
